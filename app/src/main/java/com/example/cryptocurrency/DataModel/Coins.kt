ef5b2594542d0fb2be34560a77758feeb7f583d6
Alexander Andrenko <andrenkom@gmail.com>
2022-03-19 15:16:34 +0700

    git init

--- app/src/main/java/com/example/cryptocurrency/DataModel/Coins.kt
+++ app/src/main/java/com/example/cryptocurrency/DataModel/Coins.kt
@@ -1,4 +1,23 @@
 package com.example.cryptocurrency.DataModel
 
-class Coins {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity
+data class Coins(
+    @PrimaryKey var id: String,
+    @ColumnInfo(name = "Name") var name: String,
+    @ColumnInfo(name = "Discription") var description: String,
+    @ColumnInfo(name = "GenesisDate") var genesisDate: Date,
+    @ColumnInfo(name = "CoingeckoRank") var coingeckoRank: Int,
+    @ColumnInfo(name = "CurrentPriceUSD") var currentPrice: Double,
+    @ColumnInfo(name = "PriceChangePerc1Hour") var priceCP1H: Double,
+    @ColumnInfo(name = "PriceChangePerc24Hour") var priceCP24H: Double,
+    @ColumnInfo(name = "PriceChangePerc7Days") var priceCP7D: Double,
+    @ColumnInfo(name = "PriceChangePerc14Days") var priceCP14D: Double,
+    @ColumnInfo(name = "PriceChangePerc30Days") var priceCP30D: Double,
+    @ColumnInfo(name = "PriceChangePerc60Days") var priceCP60D: Double
+){
 }
--- app/src/main/java/com/example/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null)
+        {
+            //val fragment =
+        }
+
     }
 }
