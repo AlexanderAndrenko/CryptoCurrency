1ef17e9e63e991d87e3b4e8c56a043d98c24ef0c
Alexander Andrenko <andrenkom@gmail.com>
2022-03-22 07:52:35 +0700

    test

--- app/src/main/java/com/example/cryptocurrency/CryptoCurrencyIntentApplication.kt
+++ app/src/main/java/com/example/cryptocurrency/CryptoCurrencyIntentApplication.kt
@@ -1,9 +1,11 @@
 package com.example.cryptocurrency
 
 import android.app.Application
+import com.example.cryptocurrency.Database.CryptoCurrencyRepository
 
 class CryptoCurrencyIntentApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        CryptoCurrencyRepository.initialize(this)
     }
 }
