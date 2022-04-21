985726e0cc90dc48ab2c94ddd33f3262ba76096e
Alexander Andrenko <andrenkom@gmail.com>
2022-04-21 14:56:25 +0700

    Create nav fragment

--- app/src/main/java/com/example/cryptocurrency/CryptoCurrencyIntentApplication.kt
+++ app/src/main/java/com/example/cryptocurrency/CryptoCurrencyIntentApplication.kt
@@ -6,6 +6,6 @@ import com.example.cryptocurrency.Database.CryptoCurrencyRepository
 class CryptoCurrencyIntentApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        CryptoCurrencyRepository.initialize(this)
+        //CryptoCurrencyRepository.initialize(this)
     }
 }
--- app/src/main/java/com/example/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/MainActivity.kt
@@ -9,14 +9,18 @@ import com.example.cryptocurrency.ViewModel.CryptoCurrencyListFragment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        CryptoCurrencyIntentApplication()
-        setContentView(R.layout.activity_main)
+        //CryptoCurrencyIntentApplication()
+
+
+        /*setContentView(R.layout.activity_main)
         var currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
         if (currentFragment == null)
         {
             val fragment = CryptoCurrencyListFragment.newInstance()
             supportFragmentManager.beginTransaction().add(R.id.fragment_container,fragment).commit()
-        }
+        }*/
+
+
 
 
     }
