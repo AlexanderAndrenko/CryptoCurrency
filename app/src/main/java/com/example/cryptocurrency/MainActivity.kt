cf080da41ade91facaa7c1180179847d9d0bf776
Alexander Andrenko <andrenkom@gmail.com>
2022-04-24 10:25:31 +0700

    Gathered malet of info page

--- app/src/main/java/com/example/cryptocurrency/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency/MainActivity.kt
@@ -2,12 +2,8 @@ package com.example.cryptocurrency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.cryptocurrency.APIclient.CoinGeckoClientAPI
-import com.example.cryptocurrency.Database.CryptoCurrencyRepository
-import com.example.cryptocurrency.ViewModel.CryptoCurrencyListFragment
 import com.example.cryptocurrency.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/cryptocurrency/fragments/InfoFragment.kt
+++ app/src/main/java/com/example/cryptocurrency/fragments/InfoFragment.kt
@@ -6,24 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cryptocurrency.R
+import com.example.cryptocurrency.databinding.FragmentInfoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [InfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class InfoFragment : Fragment() {
+
+    private lateinit var binding: FragmentInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+        binding = FragmentInfoBinding.inflate(layoutInflater)
+
+
+        return binding.root
     }
 }
